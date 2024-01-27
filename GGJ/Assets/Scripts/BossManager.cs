85319762ec4d0f12f1a25d635808a07a77c21117
Raton <149146535+Raton-mng@users.noreply.github.com>
2024-01-27 14:11:13 +0100

    Avancement deathwheel et deathcone (/createBall)

--- GGJ/Assets/Scripts/BossManager.cs
+++ GGJ/Assets/Scripts/BossManager.cs
@@ -5,14 +5,61 @@ using UnityEngine;
 public class BossManager : MonoBehaviour
 {
     private float _variationTime;
-    private float _random;
+    private int _random;
     private bool _isMoving;
     private bool _isLeft = true;
     private bool _canAttack = true;
+    
+    [SerializeField] GameObject lazerPrefab;
+    [SerializeField] GameObject startLazerPrefab;
+    [SerializeField] GameObject ballPrefab;
+    
+    public List<GameObject> LazerList = new List<GameObject>();
+    
+    
+    public static BossManager instance;
+    
     Vector3 _goToPosition;
     void Start()
     {
         gameObject.transform.position = new Vector3(-5, 4, 0);
+    }
+    
+    void Awake()
+    {
+        if (instance != null && instance != this)
+            Destroy(gameObject);    // Suppression d'une instance pr�c�dente (s�curit�...s�curit�...)
+
+        instance = this;
+    }
+
+    private int RandomPatern()
+    {
+        float randf = Random.Range(0f, 10f);
+        while (randf >= 10f)
+        {
+            //au cas ou on arrive sur la borne exterieur ce qui n'est pas pris en compte
+            randf = Random.Range(0f, 10f);
+        }
+        return Mathf.FloorToInt(randf);
+    }
+
+    private void DeathWheel()
+    {
+        StartCoroutine(CreateDW());
+        
+        StartCoroutine(DestroyInSeconds(2f));
+    }
+
+    private void DeathCone()
+    {
+        for (int i = 0; i < 12; i += 1)
+        {
+            for (int j = 0; i < 12; i += 1)
+            {
+                StartCoroutine(CreateBoule(1, new Vector3(), ballPrefab));
+            }
+        }
     }
 
     void Update()
@@ -47,5 +94,45 @@ public class BossManager : MonoBehaviour
                 _isMoving = false;
             }
         }
+    }
+    private IEnumerator DestroyInSeconds(float cooldown)
+    {
+        yield return new WaitForSeconds(cooldown);
+        
+        foreach(GameObject lazer in LazerList)
+        {
+            Destroy(lazer);
+        }
+    }
+
+    private IEnumerator CreateDW()
+    {
+        /*faire apparaître la préparation du lazer*/
+        
+        yield return new WaitForSeconds(2f);
+        
+        GameObject newLazer;
+        for (int i = 0; i < 2; i += 1)
+        {
+            newLazer = Instantiate(lazerPrefab, new Vector3(0,0,0), Quaternion.LookRotation(new Vector3(0, 0, 1), new Vector3(i, 1 - i, 0)) );
+            LazerList.Add(newLazer);
+        }
+        
+        foreach(GameObject lazer in LazerList)
+        {
+            lazer.GetComponent<Rigidbody2D>().angularVelocity = 25;
+        }
+    }
+
+    private IEnumerator CreateBoule(float f, Vector3 direction, GameObject ballPrefab)
+    {
+        yield return new WaitForSeconds(f);
+
+        GameObject ball = Instantiate(ballPrefab, gameObject.transform.position, gameObject.transform.rotation);
+        ball.GetComponent<Rigidbody2D>().velocity = direction;
+        
+        yield return new WaitForSeconds(0.1f);
+
+        ball.GetComponent<SpriteRenderer>().enabled = true;
     }
 }
