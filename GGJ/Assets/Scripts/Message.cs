8e4006558adb50ecae522499d4c08290598e8081
Chouquette <lydie.sts@gmail.com>
2024-01-27 10:25:42 +0100

    Message - Charactères spéciaux
    - pour un clavier AZERTY
    - accents possibles

--- GGJ/Assets/Scripts/Message.cs
+++ GGJ/Assets/Scripts/Message.cs
@@ -1,7 +1,6 @@
 using System;
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
 
 
 public class Message : MonoBehaviour
@@ -12,7 +11,7 @@ public class Message : MonoBehaviour
     private string _startBalise = "<color=red>";
     private string _endBalise = "</color>";
 
-    private string _msg = "Test";
+    private string _msg = "û ô ê";
     private int _cursor = 0;
 
     #endregion
@@ -26,11 +25,111 @@ public class Message : MonoBehaviour
          * We test if the correct answer is given
          */
         var letter = _msg.Substring(_cursor, 1);
+        
+        //The player doesn't have to wright spaces
+        if (letter == " ") 
+        {
+            _cursor += 1;
+            letter = _msg.Substring(_cursor, 1);
+        }
+        
+        //AZERTY Keybord
         foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
         {
-            if (Input.GetKeyDown(key) && key.ToString() == letter.ToUpper())
+            if (Input.GetKeyDown(key))
             {
-                _cursor += 1;
+                KeyCode newKey;
+                switch (key)
+                {
+                    case(KeyCode.A): //Q
+                        newKey = KeyCode.Q;
+                        break;
+                    case(KeyCode.Q): //A
+                        newKey = KeyCode.A;
+                        break;
+                    case(KeyCode.Z): //Z
+                        newKey = KeyCode.W;
+                        break;
+                    case(KeyCode.W): //Z
+                        newKey = KeyCode.Z;
+                        break;
+                    case(KeyCode.Semicolon): //M
+                        newKey = KeyCode.M;
+                        break; 
+                    case(KeyCode.M): //,
+                        newKey = KeyCode.Comma;
+                        break;
+                    case(KeyCode.Comma): //;
+                        newKey = KeyCode.Period;
+                        break;
+                    default:
+                        newKey = key;
+                        break;
+                }
+
+                string keyString;
+                switch (newKey.ToString())
+                {
+                    case("Comma"): //,
+                        keyString = ",";
+                        break;
+                    case("Period"): //;
+                        keyString = ";";
+                        break;
+                    case ("Alpha4"): //'
+                        keyString = "'";
+                        break;
+                    case("Alpha0"): //à
+                        keyString = "À";
+                        break;
+                    case("Alpha2"): //à
+                        keyString = "é";
+                        break;
+                    case("Alpha7"): //à
+                        keyString = "è";
+                        break;
+                    case("Slash"): //!
+                        keyString = "!";
+                        break;
+                    default:
+                        keyString = newKey.ToString();
+                        break;
+                }
+                
+                //Special character
+                if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                    && Input.GetKey(KeyCode.M))
+                {
+                    keyString = "?";
+                }
+                if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                    && Input.GetKey(KeyCode.LeftBracket)
+                    && Input.GetKey(KeyCode.I))
+                {
+                    keyString = "Ï";
+                }
+                if (Input.GetKey(KeyCode.LeftBracket)
+                    && Input.GetKey(KeyCode.U))
+                {
+                    keyString = "Û";
+                }
+                if (Input.GetKey(KeyCode.LeftBracket)
+                    && Input.GetKey(KeyCode.O))
+                {
+                    keyString = "Ô";
+                }
+                if (Input.GetKey(KeyCode.LeftBracket)
+                    && Input.GetKey(KeyCode.E))
+                {
+                    keyString = "Ê";
+                }
+                
+                
+                
+                if (keyString == letter.ToUpper())
+                {
+                    _cursor += 1;   
+                }
             }
         }
         
