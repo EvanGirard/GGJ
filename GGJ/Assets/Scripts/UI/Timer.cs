e6e297a675b677dd794982dbaff665c196b75af9
Chouquette <lydie.sts@gmail.com>
2024-01-28 18:36:43 +0100

    eheh

--- GGJ/Assets/Scripts/UI/Timer.cs
+++ GGJ/Assets/Scripts/UI/Timer.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 public class Timer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI timerText;
-    private float remainingTime = 120f;
+    private float _remainingTime = 90f;
 
 
     private void Update()
     {
-        if (remainingTime > 0)
+        if (_remainingTime > 0)
         {
-            remainingTime -= Time.deltaTime;
+            _remainingTime -= Time.deltaTime;
         }
-        else if (remainingTime <= 0 && !EndGame.win)
+        else if (_remainingTime <= 0 && !EndGame.win)
         {
-            remainingTime = 0;
+            _remainingTime = 0;
             EndGame.gameOver = true;
         }
         
-        int min = Mathf.FloorToInt(remainingTime / 60);
-        int sec = Mathf.FloorToInt(remainingTime % 60);
+        int min = Mathf.FloorToInt(_remainingTime / 60);
+        int sec = Mathf.FloorToInt(_remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", min, sec);
     }
 }
