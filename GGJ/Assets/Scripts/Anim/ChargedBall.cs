f5151ce09d1c589feaae0ba40f6f77b8356d22a1
Rapthan <151851356+Rapthan@users.noreply.github.com>
2024-01-28 13:54:38 +0100

    audio

---
+++ GGJ/Assets/Scripts/Anim/ChargedBall.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargedBall : MonoBehaviour
+{
+    private CapsuleCollider2D _collider2D;
+    private Animator _animator;
+    private int IsChargedID;
+    
+    
+}
